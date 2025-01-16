65c9af58182021376014f3caeb9677f14c473a94
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2025-01-16 12:08:32 +0100

    cbpolicyd-mta-installer.sh: New password and host parmas. Mta check.

--- cbpolicyd-mta-installer.sh
+++ cbpolicyd-mta-installer.sh
@@ -34,6 +34,9 @@ Automated cbpolicyd installer for Zimbra mta node.
 
 - No webui is installed.
 - Aimed at Zimbra 10.0.x
+
+Usage:   $0 --db-host=CBPOLICYD_DATABASE_HOST --db-password=CBPOLICYD_DATABASE_PASSWORD
+Example: $0 --db-host=127.0.0.1 --db-password=Mys3cr3t
 EOF
 }
 
@@ -41,7 +44,7 @@ function update_cbpolicyd_conf_in_settings () {
   cp -a ${CBPOLICYD_CONF_IN} ${CBPOLICYDCONF_TMP_BACKUP} # Backing up cbpolicyd.conf.in
   grep -lZr -e ".*sername=.*$" "${CBPOLICYD_CONF_IN}" | xargs -0 sed -i "s^.*sername=.*$^Username=${CBPOLICYD_DATABASE_USER}^g"
   grep -lZr -e ".*assword=.*$" "${CBPOLICYD_CONF_IN}"  | xargs -0 sed -i "s^.*assword=.*$^Password=${CBPOLICYD_PWD}^g"
-  grep -lZr -e "DSN=.*$" "${CBPOLICYD_CONF_IN}"  | xargs -0 sed -i "s^DSN=.*$^DSN=DBI:mysql:database=${CBPOLICYD_DATABASE};host=127.0.0.1;port=7306^g"
+  grep -lZr -e "DSN=.*$" "${CBPOLICYD_CONF_IN}"  | xargs -0 sed -i "s^DSN=.*$^DSN=DBI:mysql:database=${CBPOLICYD_DATABASE};host=${CBPOLICYD_DB_HOST};port=7306^g"
 }
 
 function cron_setup () {
@@ -53,11 +56,28 @@ function cron_setup () {
   fi
 }
 
+function mta_check () {
+  MTA_FOUND="FALSE"
+
+  for nserver in $(sudo su - zimbra -c 'zmprov -l getAllServers mta'); do
+    if [[ "$nserver" == "${ZMHOSTNAME}" ]] ; then
+      MTA_FOUND="TRUE"
+    fi
+  done
+
+  if [[ "${MTA_FOUND}" == "TRUE" ]] ; then
+    :
+  else
+    echo "This node needs to be a mta!"
+    echo "Aborting..."
+    exit 1
+  fi
+}
+
 # Definitions
 
 ZIMBRA_MYSQL_BINARY="/opt/zimbra/bin/mysql"
 
-CBPOLICYD_PWD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c 10) # TODO: Read from input
 CBPOLICYD_DATABASE_USER='ad-policyd_db'
 CBPOLICYD_DATABASE='policyd_db'
 
@@ -66,11 +86,39 @@ CBPOLICYD_CONF_IN='/opt/zimbra/conf/cbpolicyd.conf.in'
 
 # Main program
 
+# Check the arguments.
+for option in "$@"; do
+  case "$option" in
+    -h | --help)
+      usage
+      exit 0
+    ;;
+    --db-host=*)
+      CBPOLICYD_DB_HOST=`echo "$option" | sed 's/--db-host=//'`
+    ;;
+    --db-password=*)
+      CBPOLICYD_PWD=`echo "$option" | sed 's/--db-password=//'`
+    ;;
+  esac
+done
+
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
+
+if [[ "x$CBPOLICYD_PWD" == x ]] ; then
+  usage
+  exit 1
+fi
+
+if [[ "x$CBPOLICYD_DB_HOST" == x ]] ; then
+  usage
+  exit 1
+fi
+
+mta_check
 
 update_cbpolicyd_conf_in_settings # Update username, password and database on cbpolicyd.conf.in
 cron_setup # Setting up cron
